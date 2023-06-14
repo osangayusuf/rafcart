8bfc2c2b1b944f302bee1446e9b948796da9e0fb
Osanga Yusuf <yusufjenom@gmail.com>
2023-06-14 16:28:29 +0100

    Added new config values

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,26 @@ module.exports = {
     "./resources/**/*.vue",
   ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    screen:{
+        sm: "576px",
+        md: "768px",
+        lg: "992px",
+        xl: "1200px",
+    },
+    container: {
+        center: true,
+        padding: "1rem",
+    },
+    extend: {
+        colors: {
+            'primary': '#fd3d57',
+        },
+        fontFamily:{
+            'poppins': ["Poppins", "sans-serif"],
+            'roboto': ["Roboto", "sans-serif"]
+        }
+    },
+    },
+  // plugins: [require("@tailwindcss/forms")],
+
 }
