2428791c178224382ab29f1621d451d7b6b3bee9
Osanga Yusuf <yusufjenom@gmail.com>
2023-06-14 16:22:14 +0100

    Updated migrations

--- database/migrations/2023_05_01_142156_create_carts_table.php
+++ database/migrations/2023_05_01_142156_create_carts_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('carts', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('product_id')->unique('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('product_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
