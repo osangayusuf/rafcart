671a1f708bd9448614361273523dc8361121a7e5
Osanga Yusuf <yusufjenom@gmail.com>
2023-06-14 16:27:28 +0100

    Updated head section

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,19 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{env('APP_NAME') . '-Ecommerce Tailwind'}}</title>
-
     <link rel="shortcut icon" href="{{asset('storage/images/favicon/favicon.ico')}}" type="image/x-icon">
+
     @vite(['resources/css/app.css'])
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&family=Roboto:wght@400;500;700&display=swap"
-        rel="stylesheet">
 
     <link rel="stylesheet" href="/node_modules/@fortawesome/fontawesome-free/css/all.min.css">
-    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-
 </head>
 
 <body>
@@ -205,6 +200,7 @@
     <!-- ./copyright -->
 
     <x-flash-message/>
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </body>
 
 </html>
